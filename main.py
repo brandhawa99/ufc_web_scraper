530f0e4bd73e24961da3dc976f6fe228d9242aae
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-21 12:01:49 -0800

    finished

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+#!/usr/bin/env
 from bs4 import BeautifulSoup
 import requests
 
-
-URL = 'https://www.ufc.com/rankings'
+UFC_URL = 'https://www.ufc.com/rankings'
 LEARNING_URL = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Web+Developer&txtLocation=Canada"
-html_text = requests.get(LEARNING_URL).text
+
+# Get ufc rankings page HTML;
+html_text = requests.get(UFC_URL).text
 soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find('li', class_="clearfix job-bx wht-shd-bx")
-publish_date = jobs.find('span', class_='sim-posted').span.text.strip()
-comp = jobs.find('h3', class_="joblist-comp-name").text.replace(" ", "")
-skills = jobs.find('span', class_="srp-skills").text.strip()
-print(f'''
-Company Name: {comp}
-Required Skills: {skills}
-date: {publish_date}
-''')
+# get all the different divisions
+divisions = soup.find_all('div', class_="view-grouping-content")
+for division in divisions:
+  # Get champ div name and all the contenders
+    champ = division.find('a').text
+    division_name = division.find('h4').text
+    contenders = division.find_all("tr")
+    print('----------')
+    print(f"DIVISION: {division_name}")
+    print(f"Champion: {champ}")
+    for contender in contenders:
+      # go through contenders and find their rank name and how their position has changed
+        contender_rank = contender.find(
+            'td', class_="views-field views-field-weight-class-rank").text
+        contender_name = contender.find('div', class_="views-row").a.text
+        conteder_change = contender.find(
+            'td', class_="views-field views-field-weight-class-rank-change").text
+        print(
+            f"Rank: {contender_rank.strip()}, Name: {contender_name.strip()}, Change: {conteder_change}")
