932339c08241f2faf55150f635be52bdbb91ae06
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-20 23:39:25 -0800

    testing stuff

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from bs4 import BeautifulSoup
-import urllib.request
+import requests
 
-with urllib.request.urlopen('https://www.ufc.com/rankings') as response:
-    html = response.read()
-    soup = BeautifulSoup(html, 'lxml')
-    categories = soup.find_all('h4')
-    for cats in categories:
-        print(cats.get_text())
+
+URL = 'https://www.ufc.com/rankings'
+LEARNING_URL = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Web+Developer&txtLocation=Canada"
+html_text = requests.get(LEARNING_URL).text
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find('li', class_="clearfix job-bx wht-shd-bx")
+publish_date = jobs.find('span', class_='sim-posted').span.text.strip()
+comp = jobs.find('h3', class_="joblist-comp-name").text.replace(" ", "")
+skills = jobs.find('span', class_="srp-skills").text.strip()
+print(f'''
+Company Name: {comp}
+Required Skills: {skills}
+date: {publish_date}
+''')
