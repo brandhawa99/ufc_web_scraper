42376833060f3ceaf4d9619d2a0be3c3b25b3cbe
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-21 12:32:45 -0800

    updates

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Python BeautifulSoup UFC rankings scraping 
+# UFC rankings change scraping 
+### Using BeautifulSoup scrap the ufc ranksings website and see which ranks have been changed after the past weekend results
 - [X] Get UFC Fighter rank data
 - [ ] Create Twitter acc to tweet out data ever tudesday when it is updated
 - [ ] Add data to a database and tweet out only what has changed 
--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env
 from bs4 import BeautifulSoup
 import requests
-
+# URL for UFC rankings
 UFC_URL = 'https://www.ufc.com/rankings'
-LEARNING_URL = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Web+Developer&txtLocation=Canada"
-
 # Get ufc rankings page HTML;
 html_text = requests.get(UFC_URL).text
 soup = BeautifulSoup(html_text, 'lxml')
 # get all the different divisions
 divisions = soup.find_all('div', class_="view-grouping-content")
 for division in divisions:
-  # Get champ div name and all the contenders
+  # Get division, champion and all the contenders in that division
     champ = division.find('a').text
     division_name = division.find('h4').text
     contenders = division.find_all("tr")
@@ -19,12 +17,13 @@ for division in divisions:
     print(f"DIVISION: {division_name}")
     print(f"Champion: {champ}")
     for contender in contenders:
-      # go through contenders and find their rank name and how their position has changed
+      # go through contenders and find their rank name and how their position has changed in current division
         contender_rank = contender.find(
             'td', class_="views-field views-field-weight-class-rank").text
         contender_name = contender.find('div', class_="views-row").a.text
         conteder_change = contender.find(
             'td', class_="views-field views-field-weight-class-rank-change").text
+        # only if their rank has changed show
         if "Rank" in conteder_change or "NR" in conteder_change:
             print(
                 f"Rank: {contender_rank.strip()}, Name: {contender_name.strip()}, Change: {conteder_change}")
